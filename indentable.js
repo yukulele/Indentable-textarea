ec8032c71c8f6b143854c8943aa23c8566b23261
Clément P <yukulele@users.noreply.github.com>
2017-03-16 19:58:12 +0100

    Update indentable.js

--- indentable.js
+++ indentable.js
@@ -1,25 +1,25 @@
-function indentable(textarea, len = 2){
-  tab = Array(len + 1).join(' ')
+function indentable (textarea, len = 2) {
+  const tab = Array(len + 1).join(' ')
   textarea.addEventListener('keydown', (e) => {
-    if(e.keyCode === 9 /*tab*/){
+    if (e.keyCode === 9 /* tab */) {
       const pos = textarea.selectionStart
       const val = textarea.value
       e.preventDefault()
-      if(e.shiftKey){
+      if (e.shiftKey) {
         const firstNL = (val.lastIndexOf('\n', pos - 1) + 1)
         const start = val.slice(0, firstNL)
         let rmvLen
-        const end = val.slice(firstNL).replace(new RegExp(`^ {0,${len}}`), a => (rmvLen = a.length, ''))
+        const end = val.slice(firstNL).replace(new RegExp(`^ {0,${len}}`), (a) => (rmvLen = a.length, ''))
         textarea.value = start + end
         textarea.selectionEnd = textarea.selectionStart = pos - rmvLen
-      }else{
+      } else {
         textarea.value = val.slice(0, pos) + tab + val.slice(pos)
         textarea.selectionEnd = textarea.selectionStart = pos + len
       }
     }
   })
   textarea.addEventListener('keypress', (e) => {
-    if(e.keyCode === 13 /*enter*/){
+    if (e.keyCode === 13 /* enter */) {
       const pos = textarea.selectionStart
       const val = textarea.value
       const firstNL = (val.lastIndexOf('\n', pos) + 1)
