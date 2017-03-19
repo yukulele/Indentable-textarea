87ba19bf7f348f78aa7d33f5b9fc6e2bfc5e6ce9
Clément P <yukulele@users.noreply.github.com>
2017-03-19 16:05:28 +0100

    Update indentable.js

--- indentable.js
+++ indentable.js
@@ -3,8 +3,8 @@ function indentable(textarea, len = 2) {
   textarea.addEventListener('keypress', e => {
     const pos = textarea.selectionStart
     const val = textarea.value
-    if (e.keyCode === 9) {
-      /* tab */ e.preventDefault()
+    if (e.keyCode === 9) { /* tab */
+      e.preventDefault()
       if (e.shiftKey) {
         const firstNL = val.lastIndexOf('\n', pos - 1) + 1
         const start = val.slice(0, firstNL)
@@ -18,11 +18,12 @@ function indentable(textarea, len = 2) {
         textarea.value = val.slice(0, pos) + tab + val.slice(pos)
         textarea.selectionEnd = (textarea.selectionStart = pos + len)
       }
-    } else if (e.keyCode === 13) {
-      /* enter */ const pos = textarea.selectionStart
+    } else if (e.keyCode === 13) { /* enter */
+      const pos = textarea.selectionStart - 1
       const val = textarea.value
       const firstNL = val.lastIndexOf('\n', pos) + 1
       const tab = /^ */.exec(val.slice(firstNL))[0]
+	  console.log(tab.length, "#"+val.slice(firstNL)+"#");
       setTimeout(() => {
         const pos = textarea.selectionStart
         const val = textarea.value
