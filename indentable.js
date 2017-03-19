e370cf84e9dac979dfe7a37599672114fcdb2ed9
Clément P <yukulele@users.noreply.github.com>
2017-03-19 15:37:57 +0100

    Update indentable.js

--- indentable.js
+++ indentable.js
@@ -1,32 +1,34 @@
-function indentable (textarea, len = 2) {
+function indentable(textarea, len = 2) {
   const tab = Array(len + 1).join(' ')
-  textarea.addEventListener('keypress', (e) => {
+  textarea.addEventListener('keypress', e => {
     const pos = textarea.selectionStart
     const val = textarea.value
-    if (e.keyCode === 9 /* tab */) {
-      e.preventDefault()
+    if (e.keyCode === 9) {
+      /* tab */ e.preventDefault()
       if (e.shiftKey) {
-        const firstNL = (val.lastIndexOf('\n', pos - 1) + 1)
+        const firstNL = val.lastIndexOf('\n', pos - 1) + 1
         const start = val.slice(0, firstNL)
         let rmvLen
-        const end = val.slice(firstNL).replace(new RegExp(`^ {0,${len}}`), (a) => (rmvLen = a.length, ''))
+        const end = val
+          .slice(firstNL)
+          .replace(new RegExp(`^ {0,${len}}`), a => ((rmvLen = a.length), ''))
         textarea.value = start + end
-        textarea.selectionEnd = textarea.selectionStart = pos - rmvLen
+        textarea.selectionEnd = (textarea.selectionStart = pos - rmvLen)
       } else {
         textarea.value = val.slice(0, pos) + tab + val.slice(pos)
-        textarea.selectionEnd = textarea.selectionStart = pos + len
+        textarea.selectionEnd = (textarea.selectionStart = pos + len)
       }
-    } else if (e.keyCode === 13 /* enter */) {
-      const pos = textarea.selectionStart
+    } else if (e.keyCode === 13) {
+      /* enter */ const pos = textarea.selectionStart
       const val = textarea.value
-      const firstNL = (val.lastIndexOf('\n', pos) + 1)
+      const firstNL = val.lastIndexOf('\n', pos) + 1
       const tab = /^ */.exec(val.slice(firstNL))[0]
       setTimeout(() => {
         const pos = textarea.selectionStart
         const val = textarea.value
         textarea.value = val.slice(0, pos) + tab + val.slice(pos)
-        textarea.selectionEnd = textarea.selectionStart = pos + tab.length
-      })
+        textarea.selectionEnd = (textarea.selectionStart = pos + tab.length)
+      });
     }
-  })
+  });
 }
